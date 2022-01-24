e58400bdb9088cba1ff83e48a7435481b66089c2
hector pagliano <paglianohector@gmail.com>
2022-01-24 19:10:37 -0300

    se agrega nav bar al proyecto

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+import Navbar from './componentes/Navbar';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+const App = () => {
+  return(
+    <>
+    <Navbar />
+    </>
+  )
+};
+
+export default App;
---
+++ src/componentes/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () =>  {
+    return (
+        <nav>
+            <ul>
+                <li>inicio</li>
+                <li>inicio</li>
+                <li>inicio</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar ;
